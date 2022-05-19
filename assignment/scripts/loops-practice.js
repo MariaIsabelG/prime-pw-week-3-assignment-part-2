093f4b9c22b202e569fa7271864a5bfa2960d0e4
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-05-19 12:23:30 -0500

    created a 'for loop' to log even numbers from 2-10

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -29,7 +29,9 @@ for (let i=3; i<6; i++){
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this? 
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-
+for(let i=2; i<=10; i=i+2){
+  console.log(i)
+}
 
 
 
